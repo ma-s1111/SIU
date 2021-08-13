e1a4c6dbaa8bf05582ca11a0b9e8cf8676becc39
ma-s1111 <marwah.salama@hotmail.com>
2021-08-13 21:53:40 +0200

    A

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const navSlide = () => {
         nav.classList.toggle('nav-active');
 
         //animate links in nav
-        navLinks.forEach((link, index) => {
+        navLinks.forEach((link) => {
             if (link.style.animation) {
                 link.style.animation = ''
             }
@@ -22,3 +22,35 @@ const navSlide = () => {
 }
 
 navSlide();
+
+function loadLanguage() {
+    currentLang = localStorage.getItem('selectedLang');
+    if (currentLang == "english") {
+        inEnglish();
+    }
+    else if(currentLang == "arabic") {
+        inArabic();
+    }
+}
+
+function inArabic() {
+    const enContainer = document.querySelector(".en");
+    const arContainer = document.querySelector(".ar");
+
+    enContainer.style.display = "none";
+    arContainer.style.display = "block";
+
+    localStorage.setItem("selectedLang", "arabic");
+
+
+}
+
+function inEnglish() {
+    var enContainer = document.querySelector(".en");
+    var arContainer = document.querySelector(".ar");
+    
+    enContainer.style.display = "block";
+    arContainer.style.display = "none";
+
+    localStorage.setItem('selectedLang', 'english');
+}

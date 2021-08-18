3e6079e4a6ef1ffd2c3fd7090a0f2bb6cfb31ebc
ma-s1111 <marwah.salama@hotmail.com>
2021-08-18 23:52:45 +0200

    G

--- js/main.js
+++ js/main.js
@@ -23,6 +23,56 @@ const navSlide = () => {
 
 navSlide();
 
+const navSlide2 = () => {
+    var burger = document.querySelector('.burger2');
+    const nav = document.querySelector('.nav-links2');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', ()=>{
+        //toggle navigation bar
+        nav.classList.toggle('nav-active');
+
+        //animate links in nav
+        navLinks.forEach((link) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = "navLinkFade 0.5s ease forwards 0.5s";
+            } 
+        });
+
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide2();
+
+const navSlide3 = () => {
+    var burger = document.querySelector('.burger3');
+    const nav = document.querySelector('.nav-links3');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', ()=>{
+        //toggle navigation bar
+        nav.classList.toggle('nav-active');
+
+        //animate links in nav
+        navLinks.forEach((link) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = "navLinkFade 0.5s ease forwards 0.5s";
+            } 
+        });
+
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide3();
+
 function loadLanguage() {
     currentLang = localStorage.getItem('selectedLang');
     if (currentLang == "english") {
@@ -31,40 +81,54 @@ function loadLanguage() {
     else if(currentLang == "arabic") {
         inArabic();
     }
+    else if(currentLang == "swedish") {
+        inSwedish();
+    }
 }
 
 function inArabic() {
-    const enContainer = document.querySelector(".en");
-    const arContainer = document.querySelector(".ar");
+    var enContainer = document.querySelector(".en");
+    var arContainer = document.querySelector(".ar");
+    var svContainer = document.querySelector(".sv");
 
+    svContainer.style.display = "none";
     enContainer.style.display = "none";
     arContainer.style.display = "block";
 
     localStorage.setItem("selectedLang", "arabic");
-
-
 }
 
 function inEnglish() {
     var enContainer = document.querySelector(".en");
     var arContainer = document.querySelector(".ar");
+    var svContainer = document.querySelector(".sv");
     
     enContainer.style.display = "block";
     arContainer.style.display = "none";
+    svContainer.style.display = "none";
 
     localStorage.setItem('selectedLang', 'english');
+}
+
+function inSwedish() {
+    var enContainer = document.querySelector(".en");
+    var arContainer = document.querySelector(".ar");
+    var svContainer = document.querySelector(".sv");
+    
+    svContainer.style.display = "block";
+    enContainer.style.display = "none";
+    arContainer.style.display = "none";
+
+    localStorage.setItem('selectedLang', 'swedish');
 }
 
 function showDepartments(id) {
     const allDepartments = document.querySelectorAll(".department");
     const selectedDepartments = document.getElementById(id);
 
-
     for (let i = 0; i < allDepartments.length; i++) {
         
-        if(allDepartments[i] != selectedDepartments) {
-            allDepartments[i].style.display = "none";
-        }
+        allDepartments[i].style.display = "none";
 
         if(allDepartments[i] == selectedDepartments) {
             allDepartments[i].style.display = "block";
@@ -72,11 +136,24 @@ function showDepartments(id) {
     }
 }
 
-const faculties = document.querySelectorAll('.faculty');
-        
-faculties.forEach(faculty => {
-    faculty.addEventListener('click', function() {
-        faculties.forEach(faculty => faculty.classList.remove('active'));
+function showProgramme(id) {
+    const allProgrammes = document.querySelectorAll(".programme");
+    const selectedProgramme = document.getElementById(id);
+
+    for (let i = 0; i < allProgrammes.length; i++) {
+
+        allProgrammes[i].style.display = "none";
+
+        if(allProgrammes[i] == selectedProgramme) {
+            allProgrammes[i].style.display = "block";
+        }
+    }
+}
+
+const asideItems = document.querySelectorAll('aside li');
+asideItems.forEach(item => {
+    item.addEventListener('click', function() {
+        asideItems.forEach(item => item.classList.remove('active'));
         this.classList.add('active');
-    });
-});
+    })
+})
